86837917c4f04967dff56bdfe5d4e7c4be8c8706
Sruthikj <sruthikj22@gmail.com>
2026-06-11 00:31:43 +0530

    ✅ Revising the Select Query II [oracle] — approach1

---
+++ solutions/oracle/easy/revising-the-select-query-2/solution-approach1.sql
@@ -0,0 +1,16 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Revising the Select Query II
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    oracle
+-- Status      Accepted
+-- Submitted   2026-06-11, 12:31 a.m.
+-- ──────────────────────────────────────────────────
+
+
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
+
