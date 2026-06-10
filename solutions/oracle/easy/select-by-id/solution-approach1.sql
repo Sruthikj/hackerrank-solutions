7936755914aa764a28e593de5265bfdc8de1fc7a
Sruthikj <sruthikj22@gmail.com>
2026-06-11 00:34:49 +0530

    ✅ Select By ID [oracle] — approach1

---
+++ solutions/oracle/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,16 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    oracle
+-- Status      Accepted
+-- Submitted   2026-06-11, 12:34 a.m.
+-- ──────────────────────────────────────────────────
+
+
+
+SELECT *
+FROM CITY
+WHERE ID = 1661;
+
