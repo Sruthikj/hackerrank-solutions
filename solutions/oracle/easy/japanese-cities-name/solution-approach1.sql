af170559f2c45b18b61d400a6e4d2930bc1532a7
Sruthikj <sruthikj22@gmail.com>
2026-06-11 00:36:58 +0530

    ✅ Japanese Cities' Names [oracle] — approach1

---
+++ solutions/oracle/easy/japanese-cities-name/solution-approach1.sql
@@ -0,0 +1,17 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Japanese Cities' Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    oracle
+-- Status      Accepted
+-- Submitted   2026-06-11, 12:36 a.m.
+-- ──────────────────────────────────────────────────
+
+
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+
+
