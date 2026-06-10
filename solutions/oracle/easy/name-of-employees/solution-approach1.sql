14f61165bce74fd20d27bca5da71e6d09eacd1ba
Sruthikj <sruthikj22@gmail.com>
2026-06-11 00:50:03 +0530

    ✅ Employee Names [oracle] — approach1

---
+++ solutions/oracle/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,21 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    oracle
+-- Status      Accepted
+-- Submitted   2026-06-11, 12:50 a.m.
+-- ──────────────────────────────────────────────────
+
+
+
+SELECT name
+FROM Employee
+ORDER BY name ASC;
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
